4fe9fceac3ab1ed5076424ac0f362b7486f5fc37
dojinyou <djyou128@gmail.com>
2023-03-05 17:51:30 +0900

    exception logging 레벨 추가 redisTemplate 추가

--- src/main/kotlin/com/dojinyou/numble/timedeal/global/config/RedisConfig.kt
+++ src/main/kotlin/com/dojinyou/numble/timedeal/global/config/RedisConfig.kt
@@ -11,24 +11,26 @@ import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
-
 
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3000)
 @Configuration
 class RedisConfig(
     @Value("\${spring.data.redis.host}")
-    private val host:String,
+    private val host: String,
 
     @Value("\${spring.data.redis.host}")
-    private val port:Int,
+    private val port: Int
 ) {
 
     @Bean
     fun objectMapper(): ObjectMapper {
         val mapper = ObjectMapper()
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-        mapper.registerModules(listOf(JavaTimeModule(),Jdk8Module()))
+        mapper.registerModules(listOf(JavaTimeModule(), Jdk8Module()))
         return mapper
     }
 
@@ -37,5 +39,15 @@ class RedisConfig(
         val redisStandaloneConfiguration = RedisStandaloneConfiguration(host, port)
         redisStandaloneConfiguration.password = RedisPassword.none()
         return LettuceConnectionFactory(redisStandaloneConfiguration)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any>? {
+        val redisTemplate: RedisTemplate<String, Any> = RedisTemplate()
+        redisTemplate.connectionFactory = redisConnectionFactory()
+        redisTemplate.setEnableTransactionSupport(true)
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer(objectMapper())
+        return redisTemplate
     }
 }
