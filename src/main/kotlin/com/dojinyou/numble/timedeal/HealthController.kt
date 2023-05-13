df002da2f4b689bb04da920a18fbd857532ea185
dojinyou <djyou128@gmail.com>
2023-05-13 14:17:17 +0900

    add health controller

---
+++ src/main/kotlin/com/dojinyou/numble/timedeal/HealthController.kt
@@ -0,0 +1,13 @@
+package com.dojinyou.numble.timedeal
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+    @GetMapping("health")
+    @ResponseStatus(HttpStatus.OK)
+    fun healthCheck() { /* 헬스체크용 api */}
+}
